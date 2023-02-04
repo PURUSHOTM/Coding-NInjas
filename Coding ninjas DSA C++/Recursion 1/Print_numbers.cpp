53c5f59606a14fb980a878b6a583bba692769c03
Purushottam Kumar <87223240+PURUSHOTM@users.noreply.github.com>
2023-02-04 22:30:49 +0530

    Add files via upload

---
+++ Coding ninjas DSA C++/Recursion 1/Print_numbers.cpp
@@ -0,0 +1,26 @@
+/*Print Numbers
+Send Feedback
+Given the number 'n', write a code to print numbers from 1 to n in increasing order recursively.
+Input Format :
+Integer n
+Output Format :
+Numbers from 1 to n (separated by space)
+Constraints :
+1 <= n <= 10000
+Sample Input 1 :
+ 6
+Sample Output 1 :
+1 2 3 4 5 6
+Sample Input 2 :
+ 4
+Sample Output 2 :
+1 2 3 4
+*/
+
+void print(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+}
